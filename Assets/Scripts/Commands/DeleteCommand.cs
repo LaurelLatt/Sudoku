69eb7bc36f6272d4a3db879e8e59e28c45e0ee40
Laurel Latt <latt@chapman.edu>
2025-11-29 01:54:44 -0500

    added delete command functionality

--- Assets/Scripts/Commands/DeleteCommand.cs
+++ Assets/Scripts/Commands/DeleteCommand.cs
@@ -4,14 +4,23 @@ namespace Commands
 {
     public class DeleteCommand : Command
     {
+        private Cell cell;
+        private int newValue;
+        private int oldValue;
+        public DeleteCommand(Cell cell)
+        {
+            this.cell = cell;
+            this.newValue = 0;
+        }
+
         public override void Execute()
         {
-            
+            cell.SetValue(newValue);
         }
 
         public override void Undo()
         {
-            
+            cell.SetValue(oldValue);
         }
     }
 }
